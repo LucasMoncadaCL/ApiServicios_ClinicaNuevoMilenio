92f31fd1b18ab1e5d6b95ae2ce23ca4af8714bdb
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-25 02:35:24 -0400

    Commit inicial del proyecto

---
+++ src/main/java/com/clinicanuevomilenio/solicitudservicio/SolicitudservicioApplication.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.solicitudservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicanuevomilenio/solicitudservicio/SolicitudservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.solicitudservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
